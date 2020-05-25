3c2b75810d0af36c82dfb274a89a41228e63bc33
spacemeowx2 <spacemeowx2@gmail.com>
2020-05-25 20:24:02 +0800

    add pow

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
 import { compile } from './compiler'
 import { writeFileSync } from 'fs'
+import { Socket, createServer } from './socket'
+import { checkPow } from './pow'
 
-const cls = compile(`
-  let a = 'hello '
-  let b = 'world'
-  let hw = a + b
-  hw = a + b
-  print(hw)
-`)
-let out = Buffer.alloc(8192)
-const size = cls.write(out)
-out = out.slice(0, size)
+async function onConnection(socket: Socket) {
+  await socket.writeline(`Welcome to c0 online demo!`)
+  await socket.writeline(`Zero-featured TypeScript on JVM\n`)
+  await socket.writeline(`You can input your code and see the result!`)
+  await socket.writeline(`Code(end with empty line):`)
+  const code = []
+  for (let i = 0; i < 50; i++) {
+    const line = await socket.readline()
+    if (line === '') break
+    code.push(line)
+  }
 
-console.log('Compile complete, size:', out.byteLength)
-writeFileSync('Main.class', out)
+  let out = Buffer.alloc(8192)
+  const cls = compile(code.join('\n'))
+  const size = cls.write(out)
+  out = out.slice(0, size)
+  await socket.writeline(`Compile complete, size: ${out.byteLength}`)
+
+  writeFileSync('Main.class', out)
+}
+
+async function main() {
+  const server = createServer(checkPow(onConnection))
+  const port = process.env.PORT ?? '5000'
+  server.listen(parseInt(port), process.env.HOST ?? '127.0.0.1')
+}
+
+main().catch(e => console.error(e))
---
+++ src/pow.ts
@@ -0,0 +1,41 @@
+import { Socket } from './socket'
+import { createHash } from 'crypto'
+
+function sha256(data: string): string {
+  const hash = createHash('sha256')
+  hash.update(data, 'utf8')
+  return hash.digest().toString('hex')
+}
+
+function randomString(size: number, from: string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') {
+  let out = ''
+  for (let i = 0; i < size; i++) {
+    out += from[Math.floor(Math.random() * from.length)]
+  }
+  return out
+}
+
+function generateChallenge(difficulty: number = 0.7) {
+  const full = randomString(20)
+  const p = Math.floor(full.length * difficulty)
+  const [display, hidden] = [full.slice(0, p), full.slice(p)]
+  return [
+    `sha256('${display}' + '${'?'.repeat(hidden.length)}') == ${sha256(full)}`,
+    hidden
+  ] as const
+}
+
+export function checkPow(next: (socket: Socket) => void) {
+  return async (socket: Socket) => {
+    const [ challenge, hidden ] = generateChallenge()
+    console.log('challenge', challenge, hidden)
+    await socket.writeline(challenge)
+    const ans = await socket.readline()
+    if (hidden === ans) {
+      next(socket)
+    } else {
+      await socket.writeline(`Wrong answer, the answer is ${hidden}`)
+      await socket.close()
+    }
+  }
+}
